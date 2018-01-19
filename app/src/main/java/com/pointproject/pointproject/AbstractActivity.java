4162e11f5949e3d92df73780b09aa7532cd42100
coolsmileman <coolsmileman@gmail.com>
2018-01-19 16:50:11 +0200

    added hamburger icon

--- app/src/main/java/com/pointproject/pointproject/AbstractActivity.java
+++ app/src/main/java/com/pointproject/pointproject/AbstractActivity.java
@@ -1,31 +1,28 @@
 package com.pointproject.pointproject;
 
 
-import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.pointproject.pointproject.ui.crystals.CrystalsActivity;
 import com.pointproject.pointproject.ui.maps.MapsActivity;
 import com.pointproject.pointproject.ui.settings.SettingsActivity;
-import com.pointproject.pointproject.ui.settings.SettingsFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
-/**
- * Created by xdewnik on 30.12.2017.
- */
 
 public abstract class AbstractActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -36,8 +33,28 @@ public abstract class AbstractActivity extends AppCompatActivity implements Navi
     //TODO сделать ебалу с demins для hdpi,mdpi etc
 
     @BindView(R.id.nav_view) NavigationView navigationView;
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getContentViewId());
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        navigationView.setNavigationItemSelectedListener(this);
+
+//      display burger icon on toolbar
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+
+        setupDrawer();
+    }
 
 
     @Override
@@ -54,36 +71,18 @@ public abstract class AbstractActivity extends AppCompatActivity implements Navi
      //   overridePendingTransition(R.anim.left_in, R.anim.right_out);
     }
 
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+//        sync drawer state with burger icon state
+        mDrawerToggle.syncState();
+    }
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(getContentViewId());
-        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
-        ButterKnife.bind(this);
-        setSupportActionBar(toolbar);
-        navigationView.setNavigationItemSelectedListener(this);
-
-
-    }
-
-
-
-    private void updateNavigationBarState(){
-        int actionId = getNavigationMenuItemId();
-        selectBottomNavigationBarItem(actionId);
-    }
-
-    void selectBottomNavigationBarItem(int itemId) {
-        Menu menu = navigationView.getMenu();
-        for (int i = 0, size = menu.size(); i < size; i++) {
-            MenuItem item = menu.getItem(i);
-            boolean shouldBeChecked = item.getItemId() == itemId;
-            if (shouldBeChecked) {
-                item.setChecked(true);
-                break;
-            }
-        }
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+//        sync state when configuration changed(landscape mode, etc)
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -94,6 +93,11 @@ public abstract class AbstractActivity extends AppCompatActivity implements Navi
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+//        check if burger was clicked
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
         switch (item.getItemId()) {
             case R.id.toolbar_crystals:
                 startActivity(new Intent(this, CrystalsActivity.class));
@@ -102,10 +106,6 @@ public abstract class AbstractActivity extends AppCompatActivity implements Navi
                 return super.onOptionsItemSelected(item);
         }
     }
-
-    protected abstract int getContentViewId();
-
-    protected abstract int getNavigationMenuItemId();
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -133,9 +133,51 @@ public abstract class AbstractActivity extends AppCompatActivity implements Navi
 //                startActivity(new Intent(this, NotificationsActivity.class));
 //            }
             finish();
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            DrawerLayout drawer = findViewById(R.id.drawer_layout);
             drawer.closeDrawer(GravityCompat.START);
         }, 300);
         return true;
+    }
+
+    void selectBottomNavigationBarItem(int itemId) {
+        Menu menu = navigationView.getMenu();
+        for (int i = 0, size = menu.size(); i < size; i++) {
+            MenuItem item = menu.getItem(i);
+            boolean shouldBeChecked = item.getItemId() == itemId;
+            if (shouldBeChecked) {
+                item.setChecked(true);
+                break;
+            }
+        }
+    }
+
+    protected abstract int getContentViewId();
+
+    protected abstract int getNavigationMenuItemId();
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.addDrawerListener(mDrawerToggle);
+    }
+
+    private void updateNavigationBarState(){
+        int actionId = getNavigationMenuItemId();
+        selectBottomNavigationBarItem(actionId);
     }
 }
